bbfd1185c9f8366d847cf47891ec54a88dc4ada6
thetarkus <mike@mikemob.com>
2019-03-11 19:20:11 -0400

    Refactor Bird class

--- src/Bird.php
+++ src/Bird.php
@@ -2,39 +2,15 @@
 declare(strict_types=1);
 
 
-final class Bird extends Animal
-{
-    public $shape;
-    public $location;
+class Bird {
+    private $family;
+    private $name;
+    private $location;
 
-    public function hasWings(): bool {
-        return true;
+
+    public function __construct(string $family, string $name, string $location) {
+        $this->family = $family;
+        $this->name = $name;
+        $this->location = $location;
     }
-
-    public function canFly(): bool{
-        return true;
-    }
-
-    public static function fromCSVFile($fileName): array { 
-        if (($handle = fopen($fileName, "r")) === FALSE) {
-            fclose($handle);
-            return [];
-        }
-
-        while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            if ($ignoreNameRow == NULL) {
-                $ignoreNameRow = true;
-                continue;
-            }
-
-            $bird = new Bird($row[1], 4);
-            $bird->shape = $row[0];
-            $bird->location = $row[2];
-            $result[] = $bird;
-        }
-
-        fclose($handle);
-        return $result;
-    }
-
 }
