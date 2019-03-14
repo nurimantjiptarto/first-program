0433cb071859ae42bddc92696f906c4cc4afe4f3
thetarkus <mike@mikemob.com>
2019-03-13 22:52:36 -0400

    Add bird tests

--- tests/BirdTest.php
+++ tests/BirdTest.php
@@ -7,30 +7,15 @@ use PHPUnit\Framework\TestCase;
 final class BirdTest extends TestCase
 {
 
-    public function testBirdIsAnAnimal(): void
+    public static function birdObject(): Bird
     {
-        $this->assertInstanceOf(Animal::class, new Bird('Owl', 4));
+        return new Bird("A", "B", "C");
     }
 
-
-    public function testBirdHasWings(): void
+    public function testBirdCanBeCreated(): void
     {
-        $bird = new Bird('Owl', 4);
-        $this->assertTrue(method_exists($bird, "hasWings"));
-        $this->assertTrue($bird->hasWings());
-    }
-
-    public function testBirdCanFly(): void
-    {
-        $bird = new Bird('Owl', 4);
-        $this->assertTrue(method_exists($bird, "canFLy"));
-        $this->assertTrue($bird->canFly());
-    }
-
-    public function testBirdDoesNotHaveHands(): void
-    {
-        $bird = new Bird('Columbidae', 4);
-        $this->assertFalse(method_exists($bird, "hasHands"));
+        $bird = BirdTest::birdObject();
+        $this->assertTrue(isSet($bird));
     }
 
 }
