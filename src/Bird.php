eb5d8f493aa6231010f5448a43a0efec1337787d
nurimantjiptarto <nkt5@njit.edu>
2019-03-14 05:05:01 -0400

    Adding asTableFromCSV value

--- src/Bird.php
+++ src/Bird.php
@@ -3,34 +3,45 @@ declare(strict_types=1);
 
 
 
-class Bird {
+class Bird
+{
     private $family;
     private $name;
     private $location;
 
 
-    public function __construct(string $family, string $name, string $location) {
+    public function __construct(string $family, string $name, string $location)
+    {
         $this->family = $family;
         $this->name = $name;
         $this->location = $location;
     }
 
 
-    public function asTableRow(): string {
+    public function asTableRow(): string
+    {
         return "<tr><td>" . $this->family . "</td>" .
             "<td>" . $this->name . "</td>" .
             "<td>" . $this->location . "</td></tr>";
     }
 
 
-    public static function asTable(array $records): void {
+    public static function asTable(array $records): void
+    {
         echo "<thead><th>Family</th><th>Name</th><th>Location</th></thead><tbody>";
         foreach ($records as $bird)
             echo $bird->asTableRow();
         echo "</tbody>";
+
+
+    }
+
+    public static function asTableFromCSV(string $filename): void {
+        Bird::asTable(
+            BirdFactory::createFromCSV($filename)
+        );
     }
 }
-
 
 
 class BirdFactory {
