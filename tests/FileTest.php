d2e29fd318038e365e0d74f78d89292662c7d565
nurimantjiptarto <nkt5@njit.edu>
2019-03-14 08:21:29 -0400

    Update filetest

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -1,47 +1,38 @@
 <?php
 declare(strict_types=1);
-
 use PHPUnit\Framework\TestCase;
-
 final class FileTest extends TestCase
 {
-
-    public function testDataDirectory() {
-        $this->assertDirectoryExists('data');
+    public function testDataDirectory()
+    {
+        $this->assertDirectoryExists("data");
     }
-    public function testCSVFileExists() {
-        $this->assertFileExists('data/birdtypes.csv');
+    public function testCSVFileExists()
+    {
+        $this->assertFileExists("data/birdtypes.csv");
     }
-
-    public function testCSVIsReadable(){
-        $this-> assertFileIsReadable('data/birdtypes.csv');
+    public function testCSVIsReadable()
+    {
+        $this->assertFileIsReadable("./data/birdtypes.csv");
     }
-
-
-    public function testFileObject() {
-        $this->assertInstanceOf(File::class, new File());
+    public function testFileObject()
+    {
+        $this->assertInstanceOf(Utils\File::class, new Utils\File());
     }
-
-    public function testReadCSVtoArrayExists() {
-
-        $this->assertTrue(
-            method_exists(File::class, 'readCSVtoArray')
-        );
+    public function testReadCSVtoArrayExists()
+    {
+        $this->assertTrue(method_exists(Utils\File::class, "readCSVtoArray"));
     }
-
-    public function testReadCSVtoArray() {
-
-        $records = File::readCSVtoArray("data/birdtypes.csv",'Bird');
-        print_r($records);
-
+    public function testReadCSVtoArray()
+    {
+        $records = Utils\File::readCSVtoArray("./data/birdtypes.csv", "Bird");
+        $this->assertEquals($records[0]->BirdFamily, "Auks");
+        $this->assertEquals($records[1]->BirdName, "Black Phoebe");
+        $this->assertEquals($records[2]->BirdLocation, "United States");
     }
-
-    public function testNumberOfFileContent{
-        $this->assertCount(3, 'data/birdtypes.csv');
-    }
-
-    public function testCheckingTable(){
-        $this- >assertFileExists('src/BirdTable.csv');
-
+    public function testNumberOfFileContent()
+    {
+        $records = Utils\File::readCSVtoArray("./data/birdtypes.csv", "Bird");
+        $this->assertCount(3, $records);
     }
 }
